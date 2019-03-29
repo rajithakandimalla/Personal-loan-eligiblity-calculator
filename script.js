6e9b22aeb2b6f65b701a8c9734c7d9780cfd6448
arav <aravindsapt@gmail.com>
2019-03-29 13:23:14 +0530

    added NA if the eligible amount is below 1 lacks

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ return (str.substring(0,decpoint) + "." + str.substring(decpoint,str.length));
    var cols=5;
 // ***************************************************************************************   
 // setp1-fininding salary based elegiblity
-function salBsdEle(grossIncome)
+const salBsdEle=(grossIncome)=>
 {
   if(grossIncome >= 20000 && grossIncome <= 35000)
     {
@@ -57,18 +57,16 @@ function salBsdEle(grossIncome)
     }
 }
 
-function elgEmi(){
+const elgEmi=()=>{
    gross_income= document.getElementById("gross_in").value;
     ex_emi= document.getElementById("ex_emi").value;
    sal_base=salBsdEle(gross_income);
     elg_emi=((gross_income)*(sal_base/100)-ex_emi);
     elgEmiRound=Math.round(elg_emi);
-    // console.log(Math.elg_emi);
-    
+
 }
 
-function elegAge(eAge){
-
+const elegAge=(eAge)=>{
   if( eAge>23 && eAge< 53){
     return 5;
   }else if( eAge == 54){
@@ -84,49 +82,27 @@ function elegAge(eAge){
   }
 }
 
-function loanAmout(){
+const beLowlac=(amt)=>{
+  if(amt<100000){
+    return amt +" (NA) "
+  }else{
+    return amt;
+  }
+}
+const loanAmout=()=>{
   user_age= document.getElementById("usr_age").value;
   var roi = document.getElementById("cur_roi").value;
-
   elgEmi();
-    // console.log(roi);
     rows=elegAge(user_age);
     for( var r=1; r<=rows;r++){
       pv_years=r*12;
       loan_amt= Math.round(pv(roi,12,pv_years,-elg_emi,0.05));
-      // Eletable(roi,r,elg_emi,loan_amt);
-     //  var rowByAge=r;
-     // console.log("interest Rate" +roi);
-     // console.log("Years: " + r);
-     // console.log("For EMI: "+elgEmiRound);
-     // console.log("Loan Amount: " +loan_amt);
-     table +='<tr>';
+      table +='<tr>';
       table +='<td>'+ roi +'%</td>';
       table +='<td> For '+ r + ' Years </td>';
       table +='<td> Rs: '+ elgEmiRound +'</td>';
-      table +='<td>' + loan_amt + '</td>';
+      table +='<td>' + beLowlac(loan_amt) + '</td>';
       table +='</tr>';  
   }
      document.getElementById("tabletoshow").innerHTML="<table><tr><th>Rate</th><th>Tenure</th><th>Monthly Installments</th><th>Loan Amount</th></tr>"+ table +"</table>";
 }
-
-
-// function grun() { 
-//   // elegible emi 
-//    // This one is for creating table by r(value of row and age ) value
-//   for(var r=1; r<= rows; r++){
-//     table +='<tr>';
-//     // loop for cell for(var c=1; c<cols; c++){
-//       table +='<td>'+ roi +'%</td>';
-//       table +='<td> For '+ r + ' Years </td>';
-//       table +='<td> Rs: '+ elegibleEmi +'</td>';
-//       table +='<td>' + loan_amt + '</td>';
-//     //}//for loop cell end
-//       table +='</tr>'; 
-//      }//create table row loop end
-
-//     var tab241='<table>  <tr><th>Rate</th><th>Tenure</th><th>Monthly Installments</th><th>Loan Amount</th></tr>' + table + '</table>';
-//     document.getElementById("tabletoshow").innerHTML=tab241;
-//     // var tab221=document.write('<table>' + table + '</table>')
-// }// grun function end
-
